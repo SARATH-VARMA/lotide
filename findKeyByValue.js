7ae25842ea9440ddb9e2c72d278062ddaa80a9a3
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-05-27 12:43:38 -0400

    Finding key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+const findKeyByValue = function (obj,value) {
+  let allKeys =  Object.keys(obj);
+  for (let newKey of allKeys) {
+    if (obj[newKey] === value) {
+    return newKey; 
+    }
+  }   
+  
+  return undefined; 
+
+}
+// test
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
